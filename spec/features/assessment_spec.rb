ac445bb94dc0d4ba1586a3bbf41c5e4f9b92e71a
Zack Forbing <zackforbing@gmail.com>
2016-09-12 09:51:16 -0600

    still working on testing, not getting params

--- spec/features/assessment_spec.rb
+++ spec/features/assessment_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe SearchController, type: :controller do
-  scenario "first user story" do
+describe 'User can access the page', type: :feature do
+  scenario 'and can search his location' do
     # As a user
-    conn = Faraday.new(url: "http://localhost:3000")
-    response = conn.get '/'
+    visit root_path
 
     # When I visit "/"
     fill_in 'q', with: '80203'
     # And I fill in the search form with 80203
     click_on 'Locate'
     # And I click "Locate"
-    expect(path).to eq(search_path)
+    expect(current_path).to eq(search_path)
     # Then I should be on page "/search" with parameters visible in the url
+    expect(params.class).to eq(Hash)
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
