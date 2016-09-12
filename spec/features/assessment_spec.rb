e762854b4d15b5c2f398eaf1030563a4301ec7a8
Zack Forbing <zackforbing@gmail.com>
2016-09-12 10:05:53 -0600

    working on getting requests in postman

--- spec/features/assessment_spec.rb
+++ spec/features/assessment_spec.rb
@@ -10,9 +10,9 @@ describe 'User can access the page', type: :feature do
     # And I fill in the search form with 80203
     click_on 'Locate'
     # And I click "Locate"
-    expect(current_path).to eq(search_path)
+    expect(current_path).to eq("/search?utf8=✓&q=80203&commit=Locate")
     # Then I should be on page "/search" with parameters visible in the url
-    expect(params.class).to eq(Hash)
+    expect(page).to have_content()
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
